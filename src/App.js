1b22a3a445e07166961b6cfd1bbaecb39e2a7d47
unknown <eloso85@gmail.com>
2020-03-31 14:24:42 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
 import './App.css';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import home from './pages/home';
+import login from './pages/login'
+import signup from './pages/signup';
+import Navbar from './components/Navbar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#62727b',
+      main: '#37474f',
+      dark: '#102027',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#c158dc',
+      main: '#8e24aa',
+      dark: '#5c007a',
+      contrastText: '#000',
+    },
+    
+  },
+  typography:{
+    useNextVariant: true
+  }
+})
+
+export class App extends Component {
+  render() {
+    return (
+      <div className = "App">
+       <MuiThemeProvider theme={theme}>
+       <Router>
+        <Navbar/>{/*keeps navbar outside switch so it stays staionaary */}
+         <div className="container">
+         
+          <Switch>
+            <Route exact path ="/" component ={home}/>
+            <Route exact path ="/login" component={login}/>
+            <Route exact path ="/signup" component={signup}/>
+
+          </Switch>
+         </div>
+        </Router>
+       </MuiThemeProvider>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
