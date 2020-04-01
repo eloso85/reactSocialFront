21461db9e8ef1d5251071fef5f48c849853946a1
unknown <eloso85@gmail.com>
2020-04-01 18:50:17 -0500

    finished authicator added redux

--- src/App.js
+++ src/App.js
@@ -3,33 +3,30 @@ import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
 import './App.css';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
 
+import AuthRoute from "./util/AuthRoute"
 
 import home from './pages/home';
 import login from './pages/login'
 import signup from './pages/signup';
 import Navbar from './components/Navbar';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#62727b',
-      main: '#37474f',
-      dark: '#102027',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#c158dc',
-      main: '#8e24aa',
-      dark: '#5c007a',
-      contrastText: '#000',
-    },
-    
-  },
-  typography:{
-    useNextVariant: true
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {//exp value mutiplied 1000 and into date now it will tellu when it expire if expired logs of and back home
+    window.location.href = '/login';
+    authenticated = false;
+  } else {
+    authenticated = true;
   }
-})
+}
 
 export class App extends Component {
   render() {
@@ -42,8 +39,8 @@ export class App extends Component {
          
           <Switch>
             <Route exact path ="/" component ={home}/>
-            <Route exact path ="/login" component={login}/>
-            <Route exact path ="/signup" component={signup}/>
+            <AuthRoute exact path ="/login" component={login} authenticated={authenticated}/>
+            <AuthRoute exact path ="/signup" component={signup} authenticated={authenticated}/>
 
           </Switch>
          </div>
