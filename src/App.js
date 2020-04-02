5e18ea4042ec2fdb8a8d0fa52a8232aa9152a258
unknown <eloso85@gmail.com>
2020-04-02 10:02:35 -0500

    useraction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
@@ -12,6 +12,10 @@ import home from './pages/home';
 import login from './pages/login'
 import signup from './pages/signup';
 import Navbar from './components/Navbar';
+
+//redux
+import { Provider } from 'react-redux';
+import store from './redux/stores';
 
 const theme = createMuiTheme(themeFile);
 
@@ -31,23 +35,26 @@ if (token) {
 export class App extends Component {
   render() {
     return (
-      <div className = "App">
-       <MuiThemeProvider theme={theme}>
-       <Router>
-        <Navbar/>{/*keeps navbar outside switch so it stays staionaary */}
-         <div className="container">
-         
-          <Switch>
-            <Route exact path ="/" component ={home}/>
-            <AuthRoute exact path ="/login" component={login} authenticated={authenticated}/>
-            <AuthRoute exact path ="/signup" component={signup} authenticated={authenticated}/>
 
-          </Switch>
-         </div>
-        </Router>
-       </MuiThemeProvider>
-        
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Router>
+            <Navbar />{/*keeps navbar outside switch so it stays staionaary */}
+            <div className="container">
+
+              <Switch>
+                <Route exact path="/" component={home} />
+                <AuthRoute exact path="/login" component={login} authenticated={authenticated} />
+                <AuthRoute exact path="/signup" component={signup} authenticated={authenticated} />
+
+              </Switch>
+            </div>
+          </Router>
+        </Provider>
+
+      </MuiThemeProvider>
+
+
     )
   }
 }
